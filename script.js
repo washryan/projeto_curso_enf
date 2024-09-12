d2109dc03902d25a84e497dc80485ab2113b9506
“Washington <washr.3103@gmail.com>
2024-09-12 11:36:07 -0300

    Fix: Cores do sistema, Ajustes nos códigos e tamanho.

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
+// Controle do carrossel de imagens
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function () {
-  nextImage();
-}, 10000);
+setInterval(nextImage, 10000);
 
 function nextImage() {
-  count++;
-  if (count > 6) {
-    count = 1;
-  }
-  document.getElementById("radio" + count).checked = true;
+  count = (count % 6) + 1;
+  document.getElementById(`radio${count}`).checked = true;
 }
 
+// Controle do modo escuro
 const chk = document.getElementById("chk");
-const logoImg = document.querySelector(".logo img"); // Corrigido para selecionar a imagem diretamente
+const logoImg = document.querySelector(".logo img");
 
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 
-  // Adiciona um delay para a troca da imagem para sincronizar com a animação
   setTimeout(() => {
-    if (document.body.classList.contains("dark")) {
-      logoImg.src = "img/logodark.png"; // Caminho da logo no modo dark
-    } else {
-      logoImg.src = "img/logo.png"; // Caminho da logo no modo claro
-    }
-  }, 250); // 500ms de atraso para dar a ilusão de transição suave
+    logoImg.src = document.body.classList.contains("dark")
+      ? "img/logodark.png"
+      : "img/logo.png";
+  }, 250);
 });
 
+// Inicializa o efeito de inclinação das cartas
 VanillaTilt.init(document.querySelectorAll(".card"), {
   max: 25,
   speed: 400,
@@ -36,18 +31,14 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
   "max-glare": 0.5,
 });
 
+// Controle do botão de voltar ao topo
 window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
   const btn = document.getElementById("backToTopBtn");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-}
+  btn.style.display =
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+      ? "block"
+      : "none";
+};
 
 function topFunction() {
   document.body.scrollTop = 0;
