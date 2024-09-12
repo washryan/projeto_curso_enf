8ff6dc8f54d94bc0350c61f84f4f29c05217fb56
“Washington <washr.3103@gmail.com>
2024-09-12 00:17:59 -0300

    Add: Botão de voltar ao topo

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function() {
-    nextImage();
-}, 10000)
+setInterval(function () {
+  nextImage();
+}, 10000);
 
 function nextImage() {
-    count++;
-    if(count>6){
-        count = 1;
-    }
-    document.getElementById("radio"+count).checked = true;
+  count++;
+  if (count > 6) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
 
-const chk = document.getElementById('chk');
-const logoImg = document.querySelector('.logo img'); // Corrigido para selecionar a imagem diretamente
+const chk = document.getElementById("chk");
+const logoImg = document.querySelector(".logo img"); // Corrigido para selecionar a imagem diretamente
 
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-    
-    // Adiciona um delay para a troca da imagem para sincronizar com a animação
-    setTimeout(() => {
-        if (document.body.classList.contains('dark')) {
-            logoImg.src = 'img/logodark.png'; // Caminho da logo no modo dark
-        } else {
-            logoImg.src = 'img/logo.png'; // Caminho da logo no modo claro
-        }
-    }, 250); // 500ms de atraso para dar a ilusão de transição suave
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+
+  // Adiciona um delay para a troca da imagem para sincronizar com a animação
+  setTimeout(() => {
+    if (document.body.classList.contains("dark")) {
+      logoImg.src = "img/logodark.png"; // Caminho da logo no modo dark
+    } else {
+      logoImg.src = "img/logo.png"; // Caminho da logo no modo claro
+    }
+  }, 250); // 500ms de atraso para dar a ilusão de transição suave
 });
 
 VanillaTilt.init(document.querySelectorAll(".card"), {
-    max: 25,
-    speed: 400,
-    glare: true,
-    "max-glare": 0.5
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.5,
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const btn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
